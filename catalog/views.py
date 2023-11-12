29efaa7adfa29930bf02ec246dbce975e79dc799
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-11-13 00:57:26 +0300

    corrected def contacts()

--- catalog/views.py
+++ catalog/views.py
@@ -6,4 +6,9 @@ def index(request):
 
 
 def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'Имя: {name}\nНомер телефона: {phone}\nСообщение: {message}')
     return render(request, 'catalog/contacts.html')
