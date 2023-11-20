02d60afe552dee771d2c52c322ea4eca68ee90e5
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-11-20 15:34:22 +0300

    created model Product

--- catalog/models.py
+++ catalog/models.py
@@ -10,3 +10,12 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'категория'
         verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+    def __str__(self):
+        pass
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
