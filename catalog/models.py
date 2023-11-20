663acd5cbef83beb75f68483a4183846aa93bbfb
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-11-20 21:16:11 +0300

    filled model Product

--- catalog/models.py
+++ catalog/models.py
@@ -16,8 +16,16 @@ class Category(models.Model):
 
 
 class Product(models.Model):
+    name = models.CharField(max_length=250, verbose_name='Наименование')
+    description = models.TextField(**NULLABLE, verbose_name='Описание')
+    photo = models.ImageField(upload_to='products/', **NULLABLE, verbose_name='Изображение')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    price = models.IntegerField(verbose_name='Цена')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    last_modified_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
+
     def __str__(self):
-        pass
+        return f'{self.name}, {self.category}, {self.price}'
 
     class Meta:
         verbose_name = 'продукт'
