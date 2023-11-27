ed3c72a6aac5930c0b4f419ed03dce0ad30c2f9f
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-11-27 14:21:29 +0300

    added context to index and contacts

--- catalog/views.py
+++ catalog/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 def index(request):
-    return render(request, 'catalog/index.html')
+    context = {
+        'object_list': Product.objects.all(),
+        'title': 'My Store - Главная'
+    }
+    return render(request, 'catalog/index.html', context)
 
 
 def contacts(request):
+    context = {
+        'title': 'My Store - Контакты'
+    }
     if request.method == 'POST':
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
         print(f'Имя: {name}\nНомер телефона: {phone}\nСообщение: {message}')
-    return render(request, 'catalog/contacts.html')
+    return render(request, 'catalog/contacts.html', context)
