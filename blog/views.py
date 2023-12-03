8172eb2f775b52b727934088e32a082593eaf350
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-12-04 02:50:54 +0300

    added views_count

--- blog/views.py
+++ blog/views.py
@@ -17,6 +17,12 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
 
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_count += 1
+        self.object.save()
+        return self.object
+
 
 class PostUpdateView(UpdateView):
     model = Post
