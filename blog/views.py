1dfd7cfd936a34fc6f98653b9b0c98cb1b89cc18
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-12-04 02:28:53 +0300

    added class PostUpdateView

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
+import os
+
+from django.core.files.storage import FileSystemStorage
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, ListView, DetailView
+from django.views.generic import CreateView, ListView, DetailView, UpdateView
 
 from blog.models import Post
+from config.settings import MEDIA_ROOT, MEDIA_URL
 
 
 class PostCreateView(CreateView):
@@ -17,3 +21,9 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
     model = Post
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ('title', 'body', 'photo')
+    success_url = reverse_lazy('blog:list')
