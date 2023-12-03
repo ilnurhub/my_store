d1dfb39994dc8dae77285aa25433af793251b144
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-12-04 00:26:46 +0300

    added path to list

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from blog.apps import BlogConfig
-from blog.views import PostCreateView
+from blog.views import PostCreateView, PostListView
 
 
 app_name = BlogConfig.name
 urlpatterns = [
-    path('create/', PostCreateView.as_view(), name='create_post')
+    path('create/', PostCreateView.as_view(), name='create_post'),
+    path('', PostListView.as_view(), name='list')
 ]
