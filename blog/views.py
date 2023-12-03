ff1a9e6cb791785cadcbedb920b56178b769a8fb
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-12-04 00:26:28 +0300

    added PostListView

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 
 from blog.models import Post
 
@@ -9,3 +9,7 @@ class PostCreateView(CreateView):
     model = Post
     fields = ('title', 'body', 'photo')
     success_url = reverse_lazy('catalog:index')
+
+
+class PostListView(ListView):
+    model = Post
