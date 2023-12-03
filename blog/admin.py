de224b40cf2dd70f7719e3be20a59639c7b3c870
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-12-04 01:05:30 +0300

    added PostAdmin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'is_published', 'views_count')
+    list_filter = ('title',)
+    search_fields = ('title', 'body')
