7446a5fbd1caad48565eb7f06a2d2bdbac1cfa5c
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-12-03 23:19:10 +0300

    added model Post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'null': True, 'blank': True}
+class Post(models.Model):
+   title = models.CharField(max_length=150, verbose_name='заголовок')
+   body = models.TextField(verbose_name='содержимое')
+   slug = models.CharField(max_length=150, **NULLABLE, verbose_name='slug')
+   photo = models.ImageField(upload_to='post/', **NULLABLE, verbose_name='превью')
+   created_at = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
+   is_published = models.BooleanField(default=True, verbose_name='признак публикации')
+   views_count = models.IntegerField(default=0, verbose_name='количество просмотров')
+
+   def __str__(self):
+       return self.title
+
+   class Meta:
+       verbose_name = 'статья'
+       verbose_name_plural = 'статьи'
