d58bce005f6ff81f819c230d61673a22166a1de0
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-12-04 02:46:56 +0300

    added class PostDeleteView

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,7 @@
-import os
-
-from django.core.files.storage import FileSystemStorage
-from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, ListView, DetailView, UpdateView
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 
 from blog.models import Post
-from config.settings import MEDIA_ROOT, MEDIA_URL
 
 
 class PostCreateView(CreateView):
@@ -26,4 +21,9 @@ class PostDetailView(DetailView):
 class PostUpdateView(UpdateView):
     model = Post
     fields = ('title', 'body', 'photo')
+    success_url = reverse_lazy('blog:list')
+
+
+class PostDeleteView(DeleteView):
+    model = Post
     success_url = reverse_lazy('blog:list')
