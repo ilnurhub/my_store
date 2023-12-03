9a09b628e5071bb94fffa5d9b95eaab488a834b2
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-12-04 02:28:36 +0300

    added path edit_post

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
 from blog.apps import BlogConfig
-from blog.views import PostCreateView, PostListView, PostDetailView
+from blog.views import PostCreateView, PostListView, PostDetailView, PostUpdateView
 
 
 app_name = BlogConfig.name
 urlpatterns = [
     path('create/', PostCreateView.as_view(), name='create_post'),
     path('', PostListView.as_view(), name='list'),
-    path('view/<int:pk>/', PostDetailView.as_view(), name='view_post')
+    path('view/<int:pk>/', PostDetailView.as_view(), name='view_post'),
+    path('edit/<int:pk>/', PostUpdateView.as_view(), name='edit_post')
 ]
