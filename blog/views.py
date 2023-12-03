52a8d46d64f8e7285840745141898a86f711eb11
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-12-04 00:06:43 +0300

    added class PostCreateView

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from blog.models import Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ('title', 'body', 'photo')
+    success_url = reverse_lazy('catalog:index')
