ea89d7886dd55c5cde3442d8b424ca1cc52cda75
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-12-24 21:30:56 +0300

    added path('create/')

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import ProductListView, ProductDetailView, contacts
-
+from catalog.views import ProductListView, ProductDetailView, contacts, ProductCreateView
 
 app_name = CatalogConfig.name
 urlpatterns = [
     path('', ProductListView.as_view(), name='index'),
     path('contacts/', contacts, name='contacts'),
-    path('product/<int:pk>/', ProductDetailView.as_view(), name='product')
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product'),
+    path('create/', ProductCreateView.as_view(), name='create_product')
 ]
