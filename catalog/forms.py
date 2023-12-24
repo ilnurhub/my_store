160ff9a483167aa9ddb4122178ce6cd704899a24
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2023-12-24 22:08:16 +0300

    added validation

--- catalog/forms.py
+++ catalog/forms.py
@@ -4,6 +4,23 @@ from catalog.models import Product
 
 
 class ProductForm(forms.ModelForm):
+
+    VALIDATION_WORDS = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
+
     class Meta:
         model = Product
         fields = '__all__'
+
+    def clean_name(self):
+        cleaned_data = self.cleaned_data['name']
+        for word in self.VALIDATION_WORDS:
+            if word in cleaned_data:
+                raise forms.ValidationError('Запрещенный продукт')
+        return cleaned_data
+
+    def clean_description(self):
+        cleaned_data = self.cleaned_data['description']
+        for word in self.VALIDATION_WORDS:
+            if word in cleaned_data:
+                raise forms.ValidationError('Запрещенный продукт')
+        return cleaned_data
