502cf3c30676b8cd9732ba18458ef9a4c32ab18d
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2024-01-01 16:22:57 +0300

    command to create superuser

---
+++ users/management/commands/csu.py
@@ -0,0 +1,17 @@
+from django.core.management import BaseCommand
+
+from users.models import User
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        user = User.objects.create(
+            email='admin@my_store.com',
+            first_name='Admin',
+            last_name='Adminov',
+            is_superuser=True,
+            is_staff=True,
+            is_active=True
+        )
+        user.set_password('123qwe456rty')
+        user.save()
