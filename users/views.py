cbdaa29b3b3860e34d143149ed367cbe1810d3aa
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2024-01-02 02:29:08 +0300

    added class RegisterView

--- users/views.py
+++ users/views.py
@@ -1,6 +1,16 @@
+import random
+
+from django.conf import settings
 from django.contrib.auth.views import LoginView as BaseLoginView
 from django.contrib.auth.views import LogoutView as BaseLogoutView
-from django.shortcuts import render
+from django.core.mail import send_mail
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView
+
+from users.forms import UserRegisterForm
+from users.models import User
 
 
 class LoginView(BaseLoginView):
@@ -9,3 +19,24 @@ class LoginView(BaseLoginView):
 
 class LogoutView(BaseLogoutView):
     pass
+
+
+class RegisterView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    success_url = reverse_lazy('users:email_verify')
+    template_name = 'users/register.html'
+
+    def form_valid(self, form):
+        self.object = form.save()
+        self.object.secret_key = ''.join([str(random.randint(0, 9)) for _ in range(6)])
+        self.object.save()
+        send_mail(
+            subject='Верификация электронной почты',
+            message=f'Секретный код для верификации электронной почты: {self.object.secret_key}',
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[self.object.email]
+        )
+        return super().form_valid(form)
+
+
