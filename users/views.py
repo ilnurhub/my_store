ee89e5368677bcc771babef6431d76059b0c90df
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2024-01-02 02:56:57 +0300

    added def generate_new_password

--- users/views.py
+++ users/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.views import LoginView as BaseLoginView
 from django.contrib.auth.views import LogoutView as BaseLogoutView
 from django.core.mail import send_mail
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, UpdateView
 
@@ -66,3 +66,16 @@ class UserUpdateView(UpdateView):
 
     def get_object(self, queryset=None):
         return self.request.user
+
+
+def generate_new_password(request):
+    new_password = ''.join([str(random.randint(0, 9)) for _ in range(12)])
+    send_mail(
+        subject='Вы сменили пароль',
+        message=f'Ваш новый пароль: {new_password}',
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=[request.user.email]
+    )
+    request.user.set_password(new_password)
+    request.user.save()
+    return redirect(reverse('catalog:index'))
