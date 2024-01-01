4dd66c533859c530835301c0a472e62cd2e6d478
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2024-01-01 17:13:39 +0300

    added class LoginView

--- users/views.py
+++ users/views.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.views import LoginView as BaseLoginView
 from django.shortcuts import render
 
-# Create your views here.
+
+class LoginView(BaseLoginView):
+    template_name = 'users/login.html'
