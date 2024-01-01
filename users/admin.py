490f0bca16953ed1a86c115c83cb57edeba2e6e2
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2024-01-01 22:25:26 +0300

    added UserAdmin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'email', 'first_name', 'last_name', 'phone')
+    list_filter = ('email',)
+    search_fields = ('email', 'first_name', 'last_name')
