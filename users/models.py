34fdd2cbb34c7cc6beb3653f8575bf0cc1488442
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2024-01-01 16:15:18 +0300

    added class User

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from catalog.models import NULLABLE
+
+
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(unique=True, verbose_name='email')
+    phone = models.CharField(max_length=35, **NULLABLE, verbose_name='номер телефона')
+    country = models.CharField(max_length=250, **NULLABLE, verbose_name='страна')
+    avatar = models.ImageField(upload_to='users/', **NULLABLE, verbose_name='аватар')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
