29df5ae6daf23cd97e0b69c77124d5a3a0d26aee
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2024-01-13 22:07:48 +0300

    added LOGIN_URL

--- config/settings.py
+++ config/settings.py
@@ -137,6 +137,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+LOGIN_URL = '/users/'
+
 
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
