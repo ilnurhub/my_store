f4c52ca5256313101b167876129f95b0f9fd1d8a
ilnur khabibrakhmanov <gs1.ilnur@gmail.com>
2024-01-15 17:56:14 +0300

    added classes FullProductForm, OwnerProductForm, ModeratorProductForm, ValidationMixin

--- catalog/forms.py
+++ catalog/forms.py
@@ -10,17 +10,8 @@ class StyleFormMixin:
             field.widget.attrs['class'] = 'form-control'
 
 
-class ProductForm(StyleFormMixin, forms.ModelForm):
+class ValidationMixin:
     VALIDATION_WORDS = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
-
-    class Meta:
-        model = Product
-        fields = '__all__'
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self.fields['owner'].widget = forms.HiddenInput()
 
     def clean_name(self):
         cleaned_data = self.cleaned_data['name']
@@ -35,6 +26,35 @@ class ProductForm(StyleFormMixin, forms.ModelForm):
             if word in cleaned_data:
                 raise forms.ValidationError('Запрещенный продукт')
         return cleaned_data
+
+
+class FullProductForm(StyleFormMixin, ValidationMixin, forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['owner'].widget = forms.HiddenInput()
+
+
+class OwnerProductForm(StyleFormMixin, ValidationMixin, forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['owner'].widget = forms.HiddenInput()
+        self.fields['is_published'].widget = forms.HiddenInput()
+
+
+class ModeratorProductForm(StyleFormMixin, ValidationMixin, forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ('is_published', 'description', 'category')
 
 
 class VersionForm(StyleFormMixin, forms.ModelForm):
